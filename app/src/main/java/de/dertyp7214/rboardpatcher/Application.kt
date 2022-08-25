38c532ca3bed4d3fe65db82bdd870f2e4b301583
DerBot7214 <lengwenath1@gmail.com>
2022-08-25 12:51:47 +0200

    rboardcomponents submodule

--- app/src/main/java/de/dertyp7214/rboardpatcher/Application.kt
+++ app/src/main/java/de/dertyp7214/rboardpatcher/Application.kt
@@ -2,18 +2,22 @@
 
 package de.dertyp7214.rboardpatcher
 
-import com.google.android.material.color.DynamicColors
 import de.dertyp7214.colorutilsc.ColorUtilsC
+import de.dertyp7214.rboardcomponents.utils.ThemeUtils
 import de.dertyp7214.rboardpatcher.core.clearTmp
 import de.dertyp7214.rboardpatcher.patcher.Patcher
 import de.dertyp7214.rboardpatcher.patcher.ThemePack
+import de.dertyp7214.rboardpatcher.screens.MainActivity
 
 class Application : android.app.Application() {
     val patcher by lazy { Patcher(this) }
 
     override fun onCreate() {
         super.onCreate()
-        DynamicColors.applyToActivitiesIfAvailable(this)
+        ThemeUtils.registerActivityLifecycleCallbacks(this)
+        ThemeUtils.applyTheme(this) { activity, changed ->
+            if (activity is MainActivity && changed) activity.recreate()
+        }
 
         ColorUtilsC.init()
 
